c8edda36cdd10b43bbf0161995c9fe271dea91af
swietanurjanah <swietanurjanah@gmail.com>
2022-04-20 09:23:13 +0700

    add note

---
+++ Basic Algorithm/LoopStatement.js
@@ -0,0 +1,23 @@
+let number = 10;
+/*while (number != 0) {
+  console.log(number);
+  number--;
+}*/
+
+/*do {
+  //statement
+  console.log(number);
+  number--;
+} while (number >= 0);
+*/
+
+//for loop
+/*for (number = 10; number > 0; number--) {
+  console.log("ini for loop =", number);
+}*/
+
+//for/of Loop
+let array = ["BMW", "JAGUAR", "MERCEDES"];
+for (item of array) {
+  console.log(item);
+}
